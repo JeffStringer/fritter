30f79d5a9b3a436dbd06aa8c3b50ac7da01b7192
jeffstringer <jeff.j.stringer@gmail.com>
2014-12-19 20:35:17 -0800

    update message.html

--- app/assets/javascripts/controllers/MessagesController.js
+++ app/assets/javascripts/controllers/MessagesController.js
@@ -1,7 +1,12 @@
-messageList.controller('MessageCtrl', function MessageController($scope, MessagesFactory, Auth) {
+messageList.controller('MessageCtrl', function MessageController($scope, MessagesFactory, UsersFactory, Auth) {
   $scope.MessagesFactory = MessagesFactory;
   $scope.messages = MessagesFactory.messages;
-  $scope.newMessage = {};
+
+  $scope.UsersFactory = UsersFactory;
+
+  Auth.currentUser().then(function (user){
+    $scope.user = user;
+  });
 
   $scope.getMessages = (function() {
     MessagesFactory.getMessages()
@@ -10,11 +15,10 @@ messageList.controller('MessageCtrl', function MessageController($scope, Message
     });
   })();
 
-  $scope.addMessage = function() {
-    MessagesFactory.addMessage (MessagesFactory.message)
+  $scope.addMessage = function(user) {
+    MessagesFactory.addMessage ()
       .success(function(data) {
-        var now = moment();
-        $scope.messages.push({fweet: MessagesFactory.message}, {date: now}, {user: MessagesFactory.user});
+        $scope.messages.push({fweet: MessagesFactory.message}, {user: $scope.user});
           MessagesFactory.message = null;
         });
   }
