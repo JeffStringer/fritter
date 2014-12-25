df158dc436469d5c9d3608cca5078ad80d579a2f
jeffstringer <jeff.j.stringer@gmail.com>
2014-12-25 12:22:06 -0800

    add navigation rerouting specs

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,10 +1,19 @@
 describe 'the login process' do
-  it 'signs in a user who uses correct email and password', js: true do
+  it 'reroutes logged in user to main path', js: true do
     visit '/#/login'
     FactoryGirl.create(:user)
     fill_in 'Email', with: 'user@example.com'
     fill_in 'Password', with: 'password'
     click_button 'Log In'
     expect(page).to have_content 'Logged in as User 1'
+  end
+
+  it 'signs in a user who uses correct email and password', js: true do
+    visit '/#/login'
+    FactoryGirl.create(:user)
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log In'
+    expect(current_path).to eq '/'
   end
 end
---
+++ spec/features/navigation_spec.rb
@@ -0,0 +1,19 @@
+describe 'navigation links rerouting' do
+  it 'reroutes user to login path upon clicking login link', js: true do
+    visit '/'
+    click_link 'Log In'
+    expect(page).to have_button 'Log In'
+  end
+
+  it 'reroutes user to signup path upon clicking signup link', js: true do
+    visit '/'
+    click_link 'Sign Up'
+    expect(page).to have_button 'Register'
+  end
+
+  it 'reroutes user to main path upon clicking home link', js: true do
+    visit '/#/login'
+    click_link 'Home'
+    expect(current_path).to eq '/'
+  end
+end
