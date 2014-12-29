894e0c6fe7745cd1a0fa326c54522a3cbe92519c
jeffstringer <jeff.j.stringer@gmail.com>
2014-12-28 23:28:53 -0800

    dry up create_message_spec.rb

--- spec/features/create_message_spec.rb
+++ spec/features/create_message_spec.rb
@@ -1,32 +1,26 @@
 describe 'new message' do
-  it 'displays message content on page after user successful submission', js: true do
+
+  before do
     visit '/#/login'
     FactoryGirl.create(:user)
     fill_in 'Email', with: 'user@example.com'
     fill_in 'Password', with: 'password'
     click_button 'Log In'
+  end
+
+  it 'displays message content on page after user successful submission', js: true do
     fill_in "What's happening?", with: 'Hear ye hear ye!'
     click_button 'Submit'
     expect(page).to have_content 'Hear ye hear ye!'
   end
 
   it 'displays message content on page after user successful submission', js: true do
-    visit '/#/login'
-    FactoryGirl.create(:user)
-    fill_in 'Email', with: 'user@example.com'
-    fill_in 'Password', with: 'password'
-    click_button 'Log In'
     fill_in "What's happening?", with: ''
     click_button 'Submit'
     expect(page).to have_content "message can't be blank"
   end
 
   it 'displays message content on page after user successful submission', js: true do
-    visit '/#/login'
-    FactoryGirl.create(:user)
-    fill_in 'Email', with: 'user@example.com'
-    fill_in 'Password', with: 'password'
-    click_button 'Log In'
     fill_in "What's happening?", with: 'Sed ut perspiciatis unde omnis iste natus error 
     sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae 
     ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
