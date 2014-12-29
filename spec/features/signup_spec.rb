5d80b616b24a4e52145dd60e37348305cd123cb9
jeffstringer <jeff.j.stringer@gmail.com>
2014-12-28 22:57:51 -0800

    add spec and code for validation error if handle is blank

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -33,4 +33,24 @@ describe 'the signup process' do
     click_button 'Register'
     expect(page).to have_content 'Please enter a valid email.'
   end
+
+  it 'provides error message if username is blank', js: true do
+    fill_in 'Email', with: 'user@gmail.com'
+    fill_in 'Username', with: ''
+    fill_in 'Handle', with: 'user1'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password Confirmation', with: 'password'
+    click_button 'Register'
+    expect(page).to have_content 'Please enter your username.'
+  end
+
+  it 'provides error message if handle is blank', js: true do
+    fill_in 'Email', with: 'user@gmail.com'
+    fill_in 'Username', with: 'User 1'
+    fill_in 'Handle', with: ''
+    fill_in 'Password', with: 'password'
+    fill_in 'Password Confirmation', with: 'password'
+    click_button 'Register'
+    expect(page).to have_content 'Please enter a handle.'
+  end
 end
