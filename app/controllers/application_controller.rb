854a450240f86ff77b8e2965a0dcb88887077cdd
jeffstringer <jeff.j.stringer@gmail.com>
2014-12-28 23:21:21 -0800

    add specs, code for blank/invalid email and password upon login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   after_filter :set_csrf_cookie_for_ng
+  
   def set_csrf_cookie_for_ng
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
   end
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,7 +1,11 @@
 describe 'the login process' do
-  it 'reroutes logged in user to main path', js: true do
+
+  before do
     visit '/#/login'
     FactoryGirl.create(:user)
+  end
+
+  it 'reroutes logged in user to main path', js: true do
     fill_in 'Email', with: 'user@example.com'
     fill_in 'Password', with: 'password'
     click_button 'Log In'
@@ -9,11 +13,30 @@ describe 'the login process' do
   end
 
   it 'signs in a user who uses correct email and password', js: true do
-    visit '/#/login'
-    FactoryGirl.create(:user)
     fill_in 'Email', with: 'user@example.com'
     fill_in 'Password', with: 'password'
     click_button 'Log In'
     expect(current_path).to eq '/'
+  end
+
+  it 'displays error message if email is blank', js: true do
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: 'password'
+    click_button 'Log In'
+    expect(page).to have_content 'Please enter your email.'
+  end
+
+  it 'displays error message if email is invalid', js: true do
+    fill_in 'Email', with: 'user@'
+    fill_in 'Password', with: 'password'
+    click_button 'Log In'
+    expect(page).to have_content 'Please enter a valid email.'
+  end
+
+  it 'displays error message if email is invalid', js: true do
+    fill_in 'Email', with: 'user@gmail'
+    fill_in 'Password', with: ''
+    click_button 'Log In'
+    expect(page).to have_content 'Please enter your password.'
   end
 end
