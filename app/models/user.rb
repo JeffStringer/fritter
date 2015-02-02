054e448a9dbd4cf85523a7d5aeb3b36cb9a2044f
jeffstringer <jeff.j.stringer@gmail.com>
2015-02-02 15:31:51 -0800

    temporarily disable mail in app for user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  after_create :send_email, :subscription
+  # after_create :send_email, :subscription
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
